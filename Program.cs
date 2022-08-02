fa058d4129eaf920e9e371e65a93e86fe36cf820
unknown <vagifrg@code.edu.az>
2022-08-02 18:49:37 +0400

    creat file

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace homeworktask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 2, m = 90, i, count = 0;
+
+
+            for (i = n; i <= m; i++)
+            {
+
+                if (i % 7 == 0)
+
+
+
+                    count++;
+            }
+
+            Console.WriteLine(count);
+
+        }
+    }
+}
